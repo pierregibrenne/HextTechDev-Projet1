6d7f182979b08d74d98523e34d0a758336b58ea4
pierregibrenne <pierre.gibrenne@protonmail.com>
2024-07-04 19:01:07 +0200

    init:pwa💻

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,39 +1,6 @@
-import { defineNuxtConfig } from 'nuxt'
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@vite-pwa/nuxt'
-  ],
-  pwa: {
-    manifest: {
-      name: 'My Awesome PWA',
-      short_name: 'MyPWA',
-      lang: 'en',
-      start_url: '/',
-      display: 'standalone',
-      background_color: '#ffffff',
-      theme_color: '#4DBA87',
-      icons: [
-        {
-          src: '/icon-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '/icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    },
-    workbox: {
-      runtimeCaching: [
-        {
-          urlPattern: 'https://my-api.example.com/.*',
-          handler: 'NetworkFirst',
-          method: 'GET'
-        }
-      ]
-    }
-  }
+  devtools: { enabled: true },
+  compatibilityDate: "2024-07-04",
+  modules: ["@vite-pwa/nuxt"]
 })
