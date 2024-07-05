b9f44b99df9622416ff1693ee01a51d0de1f5350
pierregibrenne <pierre.gibrenne@protonmail.com>
2024-07-05 17:21:49 +0200

    init:pwa💻

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <template>
+<div>
   <NuxtPwaManifest />
   <NuxtPage />
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,25 +14,23 @@ export default defineNuxtConfig({
       theme_color: '#4DBA87',
       icons: [
         {
-          src: './public/icon-192x192.png',
+          src: 'icon-192x192.png',
           sizes: '192x192',
           type: 'image/png'
         },
         {
-          src: './public/icon-512x512.png',
+          src: 'icon-512x512.png',
           sizes: '512x512',
           type: 'image/png'
         }
       ]
     },
     workbox: {
-      runtimeCaching: [
-        {
-          urlPattern: 'https://my-api.example.com/.*',
-          handler: 'NetworkFirst',
-          method: 'GET'
-        }
-      ]
+      navigateFallback:'/'
+    },
+    devOptions:{
+      enabled:true,
+      type:"module"
     }
   },
 
